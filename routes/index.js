16ae3273de5cf372531579a78df1ac20b8efa618
Adam Puzio <adam@jackthreads.com>
2012-11-19 15:50:46 -0500

    Removed some console.log() in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,6 @@ exports.index = function(req, res){
 	var file = 'config/servers.js';
 	fs.exists(file, function(exists){
 		var servers = exists ? require('../config/servers').servers : {};
-		console.log(servers);
-		console.log(JSON.stringify(servers));
 		res.render('index', {title: 'Skyfall', servers: servers});
 	});
 };
